f1f2dec704a4f836acf3e1571cc0515f694865e7
Mateus De Nardo <mateusdnm@hotmail.com>
2023-08-13 16:25:05 +0200

    feat: add ip input

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 document.getElementById('createWallet').addEventListener('click', async () => {
   const seed = document.getElementById('seed').value;
   const password = document.getElementById('password').value;
+  const ip = document.getElementById('ip').value;
 
   const response = await fetch('https://wallet-manager-kkmh.onrender.com/wallet/store', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ seed, password }),
+      body: JSON.stringify({ seed, password, ip }),
   });
 
   if (response.ok) {
       alert('Wallet created successfully!');
       document.getElementById('seed').value = '';
       document.getElementById('password').value = '';
+        document.getElementById('ip').value = '';
   } else {
       alert('Failed to create wallet.');
   }
@@ -40,7 +42,7 @@ document.getElementById('retrieveWallets').addEventListener('click', async () =>
           const seed = `${wallet.seed.slice(0, 4)}...${wallet.seed.slice(-4)}`;
           const password = `${wallet.password.slice(0, 4)}...${wallet.password.slice(-4)}`;
 
-          walletsDiv.innerHTML += `<div class="mb-2"><strong>Seed:</strong> ${seed} | <strong>Password:</strong> ${password}</div>`;
+          walletsDiv.innerHTML += `<div class="mb-2"><strong>Seed:</strong> ${seed} | <strong>Password:</strong> ${password}</div> <div class="mb-2"><strong>Proxy Server IP:</strong> ${wallet.ip}</div>`;
       });
   } else {
       alert('Failed to retrieve wallets.');
